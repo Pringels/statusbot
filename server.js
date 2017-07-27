ba41581f3700a104bd361f3b21b9b53d5f4a58b7
Peter Ringelmann <peterringelmann@gmail.com>
2017-07-27 14:46:02 -0400

    Added catch for api endpoint

--- server.js
+++ server.js
@@ -1,9 +1,36 @@
 const http = require('http');
+const qs = require('querystring');
 const port = 8080;
 
 const requestHandler = (request, response) => {
-	console.log(request.url);
+        if (request.method == 'POST') {
+        var body = '';
+
+        request.on('data', function (data) {
+            body += data;
+
+            // Too much POST data, kill the connection!
+            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+            if (body.length > 1e6)
+                request.connection.destroy();
+        });
+
+        request.on('end', function () {
+            let post = qs.parse(body);
+            console.log('POST', post);
+	    let user = post.user_name;
+	    let token = post.token || '';
+		if (token !== process.env.SLACK_COMMAND_TOKEN){
+		console.log('BAD AUTH!');
+		response.end('Unauthorised');		
+}	    else {
+	response.end('Gotcha ' + user + '!');
+}
+console.log(token);			
+        });
+    } else {
 	response.end('Hello Node.js Server!');
+	}
 };
 
 const server = http.createServer(requestHandler);
